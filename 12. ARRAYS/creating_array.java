d3b257f2ebd691ba91eef7c54f2da294f9f82401
Sharul Islam <sharulislam60@gmail.com>
2023-12-30 13:36:20 +0600

    basic java

--- 12. ARRAYS/creating_array.java
+++ 12. ARRAYS/creating_array.java
@@ -4,6 +4,22 @@ public class creating_array {
 
     public static void main(String args[]){
 
+        int marks[] = new int[50];
+
+        int numbers[] = {95,76,98};
+
+        String name[] = {"shejan" , "sharul", "Araff"};
+
+        for(int i=0 ; i<name.length; i++){
+            System.out.println(name[i]);
+            System.out.println(numbers[i]);
+        }
+
+        // if array size declared and if there is no value in array then all values will be filled with zero in java
+        for(int i=0 ; i<marks.length; i++){
+            System.out.print(marks[i]);
+        }
+
        
     }
 

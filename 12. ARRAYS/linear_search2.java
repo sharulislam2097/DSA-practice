0c268717c668e3e352bcf41724e3c5b0935349fc
Sharul Islam <sharulislam60@gmail.com>
2023-12-31 20:37:52 +0600

    basic array

--- 12. ARRAYS/linear_search2.java
+++ 12. ARRAYS/linear_search2.java
@@ -1,27 +1,28 @@
 public class linear_search2 {
-    public static int checkmenu(String menu[] , String find){
 
-        for(int i=0 ; i<menu.length; i++){
-            if(menu[i]==find){
+    // function to find the key from the array and return index
+    public static int checkMenu(String menu[], String find) {
+
+        for (int i = 0; i < menu.length; i++) {
+            if (menu[i] == find) {
                 return i;
             }
         }
-
 
         return -1;
 
     }
 
-    public static void main(String args[]){
-        String menu[]= {"Dosa" , "Chola" ,"Nodles" , "Burger" ,"pizza" , "nachos"};
-
+    public static void main(String args[]) {
+        String menu[] = { "Dosa", "Chola", "Nodles", "Burger", "pizza", "nachos" };
         String find = "pizza";
-        int index = checkmenu(menu, find);
-        if(index==-1){
+
+        int index = checkMenu(menu, find);
+        if (index == -1) {
             System.out.println("Not found in menu");
         }
 
-        System.out.println("Found at index: "+index);
+        System.out.println("Found at index: " + index);
     }
 
 }

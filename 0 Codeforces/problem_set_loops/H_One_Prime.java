cb1393f0f38060cae4e51072f19d8e969b637e2a
Sharul Islam <sharulislam2097@gmail.com>
2024-01-08 02:20:01 +0600

    code forces basic

---
+++ 0 Codeforces/problem_set_loops/H_One_Prime.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class H_One_Prime {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        boolean isPrime = true;
+
+        for (int i = 2; i <= x - 1; i++) {
+            if (x % i == 0) {
+                isPrime = false;
+            }
+        }
+        if (isPrime == true) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        sc.close();
+
+    }
+}

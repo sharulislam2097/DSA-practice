74088a48147e30e291cfb4c291f42c184cf01e32
Sharul Islam <sharulislam2097@gmail.com>
2024-01-10 21:09:50 +0600

    array basic

---
+++ 12. ARRAYS/pair_in_array.java
@@ -0,0 +1,22 @@
+public class pair_in_array {
+    public static void printpairs(int numbers[]){
+        int pair =0;
+        for(int i=0 ; i<numbers.length; i++){
+            int curr = numbers[i];
+
+            for(int j=i+1 ; j<numbers.length ; j++){
+                System.out.print("("+curr+","+numbers[j]+")");
+                pair++;
+            }
+            System.out.println();
+        }
+        System.out.println("total number of pair:"+pair);
+        
+
+    }
+    public static void main(String[] args){
+        int numbers[]={2,4,6,8,10};
+
+        printpairs(numbers);
+    }
+}

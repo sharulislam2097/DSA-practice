23d17e8c6650fbe988125a7b593ee15ba3a42e0e
Sharul Islam <sharulislam2097@gmail.com>
2024-01-10 23:53:15 +0600

    array basic

---
+++ 12. ARRAYS/max_subarraysum1.java
@@ -0,0 +1,29 @@
+public class max_subarraysum1 {
+    public static void maxsubarraysum(int numbers[]){
+
+        int cursum =0;
+        int maxsum=Integer.MIN_VALUE;
+        for(int i=0 ; i<numbers.length; i++){
+            int start=i;
+            for(int j=i ; j<numbers.length; j++){
+                int end = j;
+                cursum=0;
+                for(int k=start ;k<=end; k++){
+                    cursum+=numbers[k];
+                }
+                System.out.println(cursum);
+                if(maxsum<cursum){
+                    maxsum=cursum;
+                }
+                
+            }
+            System.out.println();
+        }
+        System.out.println("maximum sum :"+maxsum);
+    }
+    public static void main(String[] args){
+        int numbers[]={1,-2,6,-1,3};
+
+        maxsubarraysum(numbers);
+    }
+}

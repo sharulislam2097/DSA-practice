6efa368ea71de02c1d61da0a79c6faa8e9ac25c0
Sharul Islam <sharulislam2097@gmail.com>
2024-01-12 16:34:45 +0600

    array basic

--- 12. ARRAYS/max_subarraysum2.java
+++ 12. ARRAYS/max_subarraysum2.java
@@ -1,3 +1,39 @@
+// printing sub array max sum using prefix sum technique
 public class max_subarraysum2 {
-    
+
+    public static void maxsubarraysum(int numbers[]) {
+        int cursum = 0;
+        int maxsum = Integer.MIN_VALUE;
+
+        // declaring prefix sum array
+        int prefix[] = new int[numbers.length];
+
+        prefix[0] = numbers[0];
+
+        for (int i = 0; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + numbers[i];
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+
+                cursum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+
+                if (maxsum < cursum) {
+                    maxsum = cursum;
+                }
+            }
+        }
+        System.out.println("max sum is:" + maxsum);
+
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { 1, -2, 6, -1, 3 };
+
+        maxsubarraysum(numbers);
+
+    }
 }

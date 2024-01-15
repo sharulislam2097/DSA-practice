2b77e0407ea60262efb392e599100284cae14bcd
Sharul Islam <sharulislam2097@gmail.com>
2024-01-16 03:09:13 +0600

    array

---
+++ 12. ARRAYS/traping_rain_water.java
@@ -0,0 +1,39 @@
+public class traping_rain_water {
+
+    public static int trappedrainwater(int height[]) {
+        int n = height.length;
+
+        // calculate left max boundary
+        int leftmax[] = new int[n];
+        leftmax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftmax[i] = Math.max(height[i], leftmax[i - 1]);
+        }
+
+        // calculate right max boundary
+
+        int rightmax[] = new int[n];
+        rightmax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightmax[i] = Math.max(height[i], rightmax[i + 1]);
+        }
+
+        int trappedwater = 0;
+        // loop
+        // waterlevel = min(leftmax bound,rightmax bound)
+        // trapped water = waterlevel-height[i];
+        for (int i = 0; i < n; i++) {
+            int waterlevel = Math.min(leftmax[i], rightmax[i]);
+            trappedwater += waterlevel - height[i];
+        }
+
+        return trappedwater;
+
+    }
+
+    public static void main(String[] args) {
+        int height[] = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+
+        System.out.println(trappedrainwater(height));
+    }
+}

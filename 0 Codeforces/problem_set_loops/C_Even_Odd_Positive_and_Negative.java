6a81fd4201009c9a734ed46513e8e536657f00b7
Sharul Islam <sharulislam2097@gmail.com>
2024-01-20 22:14:11 +0600

    problem set loops

--- 0 Codeforces/problem_set_loops/C_Even_Odd_Positive_and_Negative.java
+++ 0 Codeforces/problem_set_loops/C_Even_Odd_Positive_and_Negative.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class C_Even_Odd_Positive_and_Negative {
 
-    public static void main(String[] args) {
+    public static void main(String[]  args) {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();

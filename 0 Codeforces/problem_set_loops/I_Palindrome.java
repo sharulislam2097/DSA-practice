c7b104ee706c00edbe879073cd225219c7d8eedb
Sharul Islam <sharulislam2097@gmail.com>
2024-01-21 19:34:43 +0600

    problem set loops

---
+++ 0 Codeforces/problem_set_loops/I_Palindrome.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class I_Palindrome {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int temp = n;
+
+        int rev = 0, digit;
+
+        while (temp > 0) {
+            digit = temp % 10;
+            rev = (rev * 10) + digit;
+
+            temp = temp / 10;
+
+        }
+        if (rev == n) {
+            System.out.println(rev);
+            System.out.println("YES");
+        } else {
+            System.out.println(rev);
+            System.out.println("NO");
+        }
+
+    }
+}

234f36c685626f510479a16a2597a356c108e70f
Sharul Islam <sharulislam2097@gmail.com>
2024-01-21 19:35:37 +0600

    problem set loops

--- 0 Codeforces/problem_set_loops/I_Palindrome.java
+++ 0 Codeforces/problem_set_loops/I_Palindrome.java
@@ -5,6 +5,8 @@ public class I_Palindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        
+        sc.close();
 
         int temp =n;
 

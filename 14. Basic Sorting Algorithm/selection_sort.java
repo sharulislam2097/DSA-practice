60fd419a248efe67d65791b562c8afda445d0054
Sharul Islam <sharulislam2097@gmail.com>
2024-01-23 16:17:56 +0600

    basic sorting algorithm

---
+++ 14. Basic Sorting Algorithm/selection_sort.java
@@ -0,0 +1,30 @@
+
+public class selection_sort {
+    static void selectionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+
+        selectionSort(arr);
+    }
+}

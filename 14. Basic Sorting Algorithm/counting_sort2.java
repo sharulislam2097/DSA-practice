4a99908746a66bd32396466c82827ce7912690dd
Sharul Islam <sharulislam2097@gmail.com>
2024-01-28 13:43:13 +0600

    basic sorting algorithm

--- 14. Basic Sorting Algorithm/counting_sort2.java
+++ 14. Basic Sorting Algorithm/counting_sort2.java
@@ -1,32 +1,35 @@
-public class counting_sort {
+public class counting_sort2 {
+
     public static void countingSort(int arr[]) {
+        int n = arr.length;
         // find out range of array element
         int largest = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             largest = Math.max(largest, arr[i]);
         }
 
         // create count array
         int count[] = new int[largest + 1];
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             count[arr[i]]++;
         }
 
-        // {1, 4, 1, 3, 2, 4, 3, 7}  -arr  j
-        // {0, 2, 1, 2, 2, 0, 0, 1}  -count i
-        // {1, 1, 2, 3, 3, 4, 4, 7}  -sorted
-
-        // sorting
-        int j = 0;
-        for (int i = 0; i < count.length; i++) {
-            while (count[i] > 0) {
-                arr[j] = i;
-                j++;
-                count[i]--;
-            }
+        for (int i = 1; i < largest; i++) {
+            count[i] = count[i] + count[i - 1];
         }
 
-        for (int i = 0; i < count.length; i++) {
+        int output[] = new int[n];
+
+        // sorting
+        for (int i = n - 1; i >= 0; i--) {
+            output[--count[arr[i]]] = arr[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
 

fc67f9c2cd8e1961b5859207cbfa9d372ff9c054
Sharul Islam <sharulislam2097@gmail.com>
2024-02-02 12:40:25 +0600

    2d array

--- 15. 2D Arrays/spiral_matrix.java
+++ 15. 2D Arrays/spiral_matrix.java
@@ -1,6 +1,6 @@
 public class spiral_matrix {
 
-    public static void printSpiral(int matrix[][]) {
+    public static void printSpiral(int[][] matrix) {
 
         int startRow = 0;
         int endRow = matrix.length - 1;
@@ -43,7 +43,7 @@ public class spiral_matrix {
     }
 
     public static void main(String[] args) {
-        int matrix[][] = { { 1, 2, 3, 4 },
+        int[][] matrix = { { 1, 2, 3, 4 },
                 { 5, 6, 7, 8 },
                 { 9, 10, 11, 12 },
                 { 13, 14, 15, 16 } };

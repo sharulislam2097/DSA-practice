5269ac8bc17db272c775bf8252570cc7130c29ca
Sharul Islam <sharulislam2097@gmail.com>
2024-02-07 03:57:26 +0600

    Strings

---
+++ 16. Strings/shortest_path.java
@@ -0,0 +1,36 @@
+public class shortest_path {
+
+    public static float shortestPath(String path){
+
+        int x=0 , y=0 ; 
+
+        for(int i=0 ; i<path.length() ; i++){
+            char dir=path.charAt(i);
+
+            if(dir == 'S'){
+                y--; 
+            }
+            else if(dir == 'N'){
+                y++;
+            }
+            else if(dir == 'W'){
+                x--;
+            }
+            else{
+                x++;
+            }
+        }
+
+        int x2 = x*x; 
+        int y2= y*y ; 
+        return (float)(Math.sqrt(x2+y2));
+
+    }
+    
+
+    public static void main(String[] args){
+        String path = "WNEENESENNN";
+
+        System.out.println(shortestPath(path));
+    }
+}

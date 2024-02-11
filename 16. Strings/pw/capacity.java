8a2b6c1d6e6f4debb9e5bd0cf32b3d40723ae87c
Sharul Islam <sharulislam2097@gmail.com>
2024-02-12 02:28:34 +0600

    String extra

---
+++ 16. Strings/pw/capacity.java
@@ -0,0 +1,9 @@
+public class capacity {
+    public static void main(String[] args){
+        
+
+        StringBuffer sb= new StringBuffer("");
+        sb.append("sdfasdfasdfasdlkh");
+        System.out.println(sb.capacity());
+    }
+}

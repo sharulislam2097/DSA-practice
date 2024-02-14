e492c8d7b4e4e299acb0159d431ceda06ec539c8
Sharul Islam <sharulislam2097@gmail.com>
2024-02-14 22:04:45 +0600

    bit manipulation

--- 17. Bit Manipulation/checkpoweroftwo.java
+++ 17. Bit Manipulation/checkpoweroftwo.java
@@ -1,11 +1,14 @@
-public class isPowerof2 {
-
-    public static boolean isPoweroftwo(int n){
-        return (n&(n-1))==0 ;
+public class checkpoweroftwo {
+    public static boolean poweroftwo(int n){
+        return (n & (n-1))==0;
     }
-
     public static void main(String[] args){
-        System.out.println(isPoweroftwo(9));
+
+        System.out.println(poweroftwo(8));
+
+
     }
+
 
 }
+       
--- 17. Bit Manipulation/clear_range_of_bits.java
+++ 17. Bit Manipulation/clear_range_of_bits.java
@@ -1,6 +1,16 @@
-public class clear_range_of_bits {
-    public static void main(String[] args){
+      public class clear_range_of_bits {
+      public static int clearBitsinrange(int n , int i , int j) {
+        int a = ((~0) << (j + 1));
+        int b = (1 << i) - 1;
+        int bitmask = a | b;
+        return n & bitmask;
 
-    }
+       }
 
-}
+      public static void main(String[] args){
+
+        System.out.println(clearBitsinrange(10 , 2, 4));
+
+       }
+
+      }

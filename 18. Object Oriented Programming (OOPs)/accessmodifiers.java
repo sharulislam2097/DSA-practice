cdcc4daa5fac9db7aab597a78c1298868808b35c
Sharul Islam <sharulislam2097@gmail.com>
2024-02-18 22:33:01 +0600

    OOP concept

---
+++ 18. Object Oriented Programming (OOPs)/accessmodifiers.java
@@ -0,0 +1,26 @@
+public class accessmodifiers {
+
+    public static void main(String[] args){
+
+
+        BankAccount myacc = new BankAccount();
+
+        myacc.username = "Persons name";
+        myacc.setPassword("mypasswordisnull");
+
+        System.out.println(myacc.username);
+    }
+    
+}
+
+
+class BankAccount{
+    public String username;
+    private String password;
+
+    public void setPassword(String pwd){
+        password = pwd;
+    }
+
+    
+}

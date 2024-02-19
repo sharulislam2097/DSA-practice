adc5aad5d67f073cc8a633cdfde3fd71c318caf4
Sharul Islam <sharulislam2097@gmail.com>
2024-02-20 03:19:51 +0600

    OOP concepts

--- 18. Object Oriented Programming (OOPs)/Constructors.java
+++ 18. Object Oriented Programming (OOPs)/Constructors.java
@@ -1,3 +1,31 @@
 public class Constructors {
-    
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+        Student s2= new Student ("sharul");
+        Student s3= new Student(546);
+
+        System.out.println(s2.name);
+        System.out.println(s3.roll);
+
+    }
+
+    // class
+
+    static class Student {
+        String name;
+        int roll;
+
+        // non parameterized constructor
+        Student() {
+            System.out.println("constructor is called");
+        }
+        // parameterized constructor
+        Student(String name) {
+            this.name = name;
+        }
+        Student(int roll){
+            this.roll =roll;
+        }
+    }
 }

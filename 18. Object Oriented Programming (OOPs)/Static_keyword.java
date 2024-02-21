33d5479754059187fb61bc3750ea74e004bc8353
Sharul Islam <sharulislam2097@gmail.com>
2024-02-21 19:58:16 +0600

    OOP concepts

--- 18. Object Oriented Programming (OOPs)/Static_keyword.java
+++ 18. Object Oriented Programming (OOPs)/Static_keyword.java
@@ -1,13 +1,13 @@
 public class Static_keyword {
     public static void main(String[] args) {
-        Student s1= new Student();
-        s1.schoolName="JVMC";
+        Student s1 = new Student();
+        s1.schoolName = "JVMC";
 
         Student s2 = new Student();
 
         System.out.println(s2.schoolName);
 
-        Student s3= new Student();
+        Student s3 = new Student();
         s3.schoolName = "Mula";
         System.out.println(s2.schoolName);
 
@@ -15,23 +15,24 @@ public class Static_keyword {
     }
 }
 
-class Student{
+class Student {
     String name;
     int roll;
 
     //static function
-    static int percentageCal(int phy , int math ,int chem ){
-        return (phy+math+chem)/3;
+    static int percentageCal(int phy, int math, int chem) {
+        return (phy + math + chem) / 3;
     }
 
 
     //Static school name for all student.
     static String schoolName;
 
-    void setName(String name){
-        this.name= name;
+    void setName(String name) {
+        this.name = name;
     }
-    String getName(){
+
+    String getName() {
         return this.name;
     }
 

32b30d7f8c6b60b2d3ff75d383d73ee3132a6b39
Sharul Islam <sharulislam2097@gmail.com>
2024-02-27 21:22:48 +0600

    Recursions

---
+++ 19. Recursion Basics/Check_array_sorted_ornot.java
@@ -0,0 +1,20 @@
+public class Check_array_sorted_ornot {
+    public static boolean isSorted(int[] arr, int i){
+        if(i==arr.length-1){
+            return true;
+        }
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+
+        return isSorted(arr, i+1);
+
+
+    }
+    public static void main(String[] args){
+        int[] arr ={1,2,3,4,5};
+        int i=0;
+
+        System.out.println(isSorted(arr , i));
+    }
+}

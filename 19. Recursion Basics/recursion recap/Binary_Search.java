6ec783d8e641f1ee4466046cc85d2e1c4dff9ae4
Sharul Islam <sharulislam2097@gmail.com>
2024-03-21 01:36:48 +0600

    recursion practice

--- 19. Recursion Basics/recursion recap/Binary_Search.java
+++ 19. Recursion Basics/recursion recap/Binary_Search.java
@@ -1,27 +1,34 @@
 public class Binary_Search {
-    public static boolean binarySearch(int[] arr, int s, int e, int key) {
+
+    public static boolean binarySearch(int[] arr, int key, int start, int end) {
+
         //base case
-        if (s > e) {
+        if (start > end) {
             return false;
         }
-        int mid = s + (e - s) / 2;
 
+        int mid = start + (end - start) / 2;
+
+        //main case
         if (arr[mid] == key) {
             return true;
         }
 
         if (arr[mid] < key) {
-            return binarySearch(arr, mid + 1, e, key);
+            return binarySearch(arr, key, mid + 1, end);
+
         } else {
-            return binarySearch(arr, s, mid - 1, key);
+            return binarySearch(arr, key, start, mid - 1);
         }
+
 
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 4, 6, 10, 14, 16};
-        int key = 23;
+        int[] arr = {2, 4, 6, 8, 12, 14, 16, 18};
+        int key = 14;
 
-        System.out.println(binarySearch(arr, 0, arr.length - 1, key));
+        System.out.println(binarySearch(arr, key, 0, arr.length - 1));
+
     }
 }
